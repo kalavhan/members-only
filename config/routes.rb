21f0d3c53a65ec7e6cde8e3a0f3d04be46931f6c
kalavhan <josue.brigido1@gmail.com>
2020-01-17 14:46:34 -0700

    Finish signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+
   root 'posts#index'
   resources :posts, only: [:new, :create, :index]
+  resources :users, only: [:new, :create]
 end
